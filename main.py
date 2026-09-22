fa4dcfe392b09d88adec81ee82c94da196db6a22
leeyjygfamily-design <leeyj.ygfamily@gmail.com>
2026-09-22 14:56:29 +0900

    페이지 이동 오류 수정 main.py

--- main.py
+++ main.py
@@ -1,40 +1,34 @@
 import streamlit as st
 
+# 페이지 설정
 st.set_page_config(
     page_title="BioLab Analyzer",
     page_icon="🧬",
     layout="wide"
 )
 
-st.title("🧬 BioLab Analyzer")
-st.subheader("생체분자 데이터 분석 프로그램")
-
-st.write(
-    "생명공학 실험에서 얻은 DNA 서열과 "
-    "BCA 단백질 정량 데이터를 분석할 수 있습니다."
+# 분석 페이지 등록
+dna_page = st.Page(
+    "pages/1_DNA.py",
+    title="DNA 분석",
+    icon="🧬"
 )
 
-st.divider()
+bca_page = st.Page(
+    "pages/2_BCA_protein.py",
+    title="BCA 단백질 정량",
+    icon="🧪"
+)
 
-col1, col2 = st.columns(2)
+# 페이지 이동 메뉴 만들기
+pg = st.navigation(
+    {
+        "BioLab Analyzer": [
+            dna_page,
+            bca_page
+        ]
+    }
+)
 
-with col1:
-    st.subheader("🧬 DNA 서열 분석")
-    st.write(
-        "DNA 염기 조성, GC 함량, "
-        "상보적 서열 등을 분석합니다."
-    )
-
-    if st.button("DNA 분석 시작 →", use_container_width=True):
-        st.switch_page("pages/1_DNA.py")
-
-
-with col2:
-    st.subheader("🧪 BCA 단백질 정량")
-    st.write(
-        "BCA 실험 데이터를 이용하여 "
-        "미지 시료의 단백질 농도를 계산합니다."
-    )
-
-    if st.button("BCA 분석 시작 →", use_container_width=True):
-        st.switch_page("pages/2_BCA_protein.py")
+# 현재 선택된 페이지 실행
+pg.run()
